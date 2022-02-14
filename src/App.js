e2e74e2c7b4c94738014acbfd36bf88e880ed6be
sareum <rkdtkfma@gmail.com>
2022-02-14 20:02:07 +0900

    Add css module to apply root css

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <p>
+        Edit and save to reload.
+      </p>
     </div>
   );
 }
