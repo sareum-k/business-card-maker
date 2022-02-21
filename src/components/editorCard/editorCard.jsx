3077ccc39886e6152226c14c2c823e273cd84496
sareum <rkdtkfma@gmail.com>
2022-02-21 21:24:20 +0900

    Add editcard from component

--- src/components/editorCard/editorCard.jsx
+++ src/components/editorCard/editorCard.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
 import styles from './editorCard.module.css';
+import Button from '../button/button';
+import ImageFileInput from '../image_file_input/imageFileInput';
 
 const EditorCard = ({ card }) => {
+    const { name, company, title, email, message, theme, fileName, fileURL } = card;
+
+    const onSubmit = () => {
+
+    };
+
     return (
-        <table className={styles.table}>
-            <tr>
-                <td><input className={styles.input} type="text" placeholder="Name" />{card.name}</td>
-                <td><input className={styles.input} type="text" placeholder="Company" /></td>
-                <td>
-                    <select className={styles.select}>
-                        <option value="Dark">Dark</option>
-                        <option value="Light">Light</option>
-                        <option value="Colorful">Colorful</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td><input className={styles.input} type="text" placeholder="Title" /></td>
-                <td><input className={styles.input} type="text" placeholder="Email" /></td>
-            </tr>
-            <tr>
-                <td><textarea className={styles.text} placeholder="Message" /></td>
-            </tr>
-            <tr>
-                <td><button>No file</button></td><td><button>Add</button></td>
-            </tr>
-        </table>
+        <form className={styles.form}>
+            <input className={styles.input} type="text" name="name" value={name} />
+            <input className={styles.input} type="text" name="company" value={company} />
+            <select className={styles.select} name="theme" value={theme}>
+                <option value="dark">Dark</option>
+                <option value="light">Light</option>
+                <option value="colorful">Colorful</option>
+            </select>
+            <input className={styles.input} type="text" name="title" value={title} />
+            <input className={styles.input} type="text" name="email" value={email} />
+            <textarea className={styles.textarea} name="message" value={message}></textarea>
+            <div className={styles.fileInput}>
+                <ImageFileInput className={styles.fileInput} />
+            </div>
+            <Button className={styles.button} name="Delete" onClick={onSubmit} />
+        </form>
     );
 }
 
---
+++ src/components/image_file_input/imageFileInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import styles from './ImageFileInput.module.css';
+
+const ImageFileInput = (props) => {
+    return (
+        <button>Image</button>
+    );
+}
+
+export default ImageFileInput;
